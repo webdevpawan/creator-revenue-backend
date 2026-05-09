abd8931a81bf6092d5090cfe3afa8973adad8655
Pawan Singh <pawansinghps9867@gmail.com>
2026-05-09 13:56:14 +0530

    Merge remote changes

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post("/logout", (req, res) => {
   });
 });
 
-// Server Start
+// Server 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
