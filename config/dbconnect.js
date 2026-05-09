1f72c15635834724920c478883d58e6661bb2238
Pawan Singh <pawansinghps9867@gmail.com>
2026-05-09 14:48:05 +0530

    Add SSL for Aiven MySQL

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -6,12 +6,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // Create MySQL Connection Pool
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false  // Required for Aiven
+  }
+})
 
 // Test Connection
 db.getConnection()
