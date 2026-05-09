9a116f85c00c900436cfc7851b1d21da0bfa5ab6
Pawan Singh <pawansinghps9867@gmail.com>
2026-05-09 15:04:58 +0530

    changed ssl

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -11,7 +11,7 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: true
   }
 });
 
