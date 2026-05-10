a6309fab3743a2a062c735223b8c6874a5c2412d
Pawan Singh <pawansinghps9867@gmail.com>
2026-05-10 11:28:35 +0530

    fixed file upload issue

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,21 +1,28 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+
+const uploadPath =   path.join(__dirname, '../uploads');
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath);
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, uploadPath);
   },
-
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(
+      null,
+      Date.now() + path.extname(file.originalname)
+    );
   }
 });
 
 const upload = multer({
-  storage,
-  limits: {
-    fileSize: 5 * 1024 * 1024
-  }
+  storage
 });
 
 module.exports = upload;
