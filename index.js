13507bed7cbc5f53de6ba276f49161e96cbc2c51
Pawan Singh <pawansinghps9867@gmail.com>
2026-05-10 15:44:50 +0530

    removed cors

--- index.js
+++ index.js
@@ -15,20 +15,28 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-  "http://localhost:4200",
-  "https://creator-revenue-frontend-o9xun3xg2-webdevpawans-projects.vercel.app",
-  "https://creator-revenue-frontend-3la1ld9jf-webdevpawans-projects.vercel.app/",
-  "https://creator-revenue-frontend.vercel.app" // add your main vercel domain too
-];
+// const allowedOrigins = [
+//   "http://localhost:4200",
+//   "https://creator-revenue-frontend-o9xun3xg2-webdevpawans-projects.vercel.app",
+//   "https://creator-revenue-frontend-3la1ld9jf-webdevpawans-projects.vercel.app/",
+//   "https://creator-revenue-frontend.vercel.app" // add your main vercel domain too
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true
+// }));
+
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+  origin: function(origin, callback) {
+    callback(null, true);
   },
   credentials: true
 }));
